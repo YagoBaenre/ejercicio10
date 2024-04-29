f19e28725ab523727f35b73f24ad1062526f5348
yago.patinogarcia <yago.patinogarcia@B05P214B.wirtz.local>
2024-04-29 21:38:43 +0200

    Apartado I

--- src/main/java/tarea10/NewMain.java
+++ src/main/java/tarea10/NewMain.java
@@ -10,6 +10,6 @@ package tarea10;
  */
 public class NewMain {
  public static void main(String[] args) {
- System.out.println(Sumador.sumar (15f,4f));
+ System.out.println(Sumador.sumar (16f, 4f));
 }}
 
