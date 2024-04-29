81be3396afa26c4eb8c0d9040b7286ae7332a8e8
yago.patinogarcia <yago.patinogarcia@B05P214B.wirtz.local>
2024-04-29 21:28:37 +0200

    Apartado D

--- src/main/java/tarea10/NewMain.java
+++ src/main/java/tarea10/NewMain.java
@@ -10,6 +10,6 @@ package tarea10;
  */
 public class NewMain {
  public static void main(String[] args) {
- System.out.println(Sumador.sumar (15f,4f));
+ System.out.println(Restador.restar (15f,4f));
 }}
 
---
+++ src/main/java/tarea10/Restador.java
@@ -0,0 +1,10 @@
+package tarea10;
+
+/**
+ *
+ * @author yago.patinogarcia
+ */
+public class Restador {
+ public static float restar (float a, float b ){
+ return a - b;
+}}
