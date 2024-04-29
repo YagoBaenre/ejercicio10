8826bc0a1d980db9879ee3c959a3cfd0c75e35cb
yago.patinogarcia <yago.patinogarcia@B05P214B.wirtz.local>
2024-04-29 20:55:05 +0200

    ApartadoB

---
+++ src/main/java/tarea10/NewMain.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tarea10;
+
+/**
+ *
+ * @author yago.patinogarcia
+ */
+public class NewMain {
+ public static void main(String[] args) {
+ System.out.println(Sumador.sumar (15f,4f));
+}}
+
---
+++ src/main/java/tarea10/Sumador.java
@@ -0,0 +1,11 @@
+package tarea10;
+
+/**
+ *
+ * @author yago.patinogarcia
+ */
+
+public class Sumador {
+ public static float sumar (float a, float b ) {
+return a + b;
+}}
